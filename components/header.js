2d4d06db62401fce003321d08d4428275210c29c
Juan Santa <jcsantahurtado@gmail.com>
2022-02-09 15:30:46 -0500

    Agregando nuevos componentes a mi página web, header y footer

---
+++ components/header.js
@@ -0,0 +1,51 @@
+const headerTemplate = document.createElement('template');
+
+headerTemplate.innerHTML = `
+  <style>
+    nav {
+      *height: 40px;
+      height: 80px;
+      display: flex;
+      align-items: center;
+      *justify-content: center;
+      *background-color:  #0a0a23;
+    }
+
+    section {
+      margin-left: 40px;
+    }
+    
+    a {
+      font-weight: 700;
+      margin: 0 25px;
+      color: #fff;
+      text-decoration: none;
+    }
+    
+    a:hover {
+      padding-bottom: 5px;
+      box-shadow: inset 0 -2px 0 0 #fff;
+    }
+  </style>
+  <header>
+    <nav>
+      <section class="logo">
+        <a href="https://app.aluracursos.com/"><img src="images/logo.png" alt="logo"></a>
+      </section>
+    </nav>
+  </header>
+`;
+
+class Header extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const shadowRoot = this.attachShadow({ mode: 'closed' });
+
+    shadowRoot.appendChild(headerTemplate.content);
+  }
+}
+
+customElements.define('header-component', Header);
