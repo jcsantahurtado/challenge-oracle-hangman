5d3d199c56faaef63f3f50e0798650b9a01fc918
Juan Santa <jcsantahurtado@gmail.com>
2022-02-09 15:30:46 -0500

    Agregando nuevos componentes a mi página web, header y footer

---
+++ components/footer.js
@@ -0,0 +1,71 @@
+const footerTemplate = document.createElement('template');
+
+footerTemplate.innerHTML = `
+  <style>
+    footer {
+      height: 60px;
+      padding: 0 10px;
+      list-style: none;
+      display: flex;
+      flex-shrink: 0;
+      justify-content: space-between;
+      align-items: center;
+      *background-color: #dfdfe2;
+    }
+
+    ul {
+      padding: 0;
+    }
+    
+    ul li {
+      list-style: none;
+      display: inline;
+    }
+    
+    a {
+      margin: 0 15px;
+      color: inherit;
+      text-decoration: none;
+    }
+    
+    a:hover {
+      padding-bottom: 5px;
+      box-shadow: inset 0 -2px 0 0 #333;
+    }
+    
+    .social-row {
+      font-size: 20px;
+    }
+    
+    .social-row li a {
+      margin: 0 15px;
+    }
+  </style>
+  <footer>
+    <a href="https://app.aluracursos.com/user/jcsantahurtado">Juan Santa, Dev en formación, alura LATAM</a>
+    <ul class="social-row">
+      <li><a href="https://github.com/jcsantahurtado"><i class="fab fa-github"></i></a></li>
+      <li><a href="https://twitter.com/jcsantahurtado"><i class="fab fa-twitter"></i></a></li>
+      <li><a href="https://www.linkedin.com/in/jcsantahurtado"><i class="fab fa-linkedin"></i></a></li>
+    </ul>
+  </footer>
+`;
+
+class Footer extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const fontAwesome = document.querySelector('link[href*="font-awesome"]');
+    const shadowRoot = this.attachShadow({ mode: 'closed' });
+
+    if (fontAwesome) {
+      shadowRoot.appendChild(fontAwesome.cloneNode());
+    }
+
+    shadowRoot.appendChild(footerTemplate.content);
+  }
+}
+
+customElements.define('footer-component', Footer);
